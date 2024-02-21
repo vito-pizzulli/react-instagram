cf7c2a55da8ce742e7b419ffe13bef6657943096
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-02-21 16:59:28 +0100

    Implemented React Router to change views based on authentication status.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,42 @@
-import '../style.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Header from './Header';
+import Home from './Home';
+import Authenticate from './Authenticate';
+import '../style.css';
 
 function App() {
+  const [isAuthenticated, setAuthenticated] = useState();
+
+  const getStatus = async () => {
+    try {
+        const response = await fetch('http://localhost:3001/api/status');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setAuthenticated(data.isAuthenticated);
+    } catch (error) {
+        console.error("Error during API call:", error);
+    }
+  };
+
+useEffect(() => {
+    getStatus();
+}, []);
+
   return (
-    <div>
-      <Header />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/authenticate" replace />} />
+            <Route path="/authenticate" element={!isAuthenticated ? <Authenticate /> : <Navigate to="/" replace />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
