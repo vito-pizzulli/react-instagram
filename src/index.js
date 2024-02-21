6816e73d71b3019b24a0bbf395a4f491b616d7a3
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-02-21 11:39:42 +0100

    Bootstrap installed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from './components/App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
