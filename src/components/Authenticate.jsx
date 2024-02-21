0c17aa9191955b39838f91e4e56c85ff3f4b2393
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-02-21 17:01:20 +0100

    Created Home & Authenticate components containing placeholder text.

---
+++ src/components/Authenticate.jsx
@@ -0,0 +1,12 @@
+import '../style.css';
+
+function Authenticate() {
+    return (
+        <div>
+            <h1>Login Form</h1>
+            <h1>Registration Form</h1>
+        </div>
+    );
+}
+
+export default Authenticate;
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import '../style.css';
+
+function Home() {
+    return (
+        <div>
+            <h1>Homepage</h1>
+        </div>
+    );
+}
+
+export default Home;
