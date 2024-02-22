20194797d18a08716ec9162a3151deb14d485d4e
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-02-22 16:26:26 +0100

    Implemented user logout with navigation and State update on success.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,34 @@
+import { useNavigate } from 'react-router-dom';
 import '../style.css';
 
-function Home() {
+function Home({ authenticatedFalse }) {
+    const navigate = useNavigate();
+
+    const handleLogout = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:3001/api/logout', { method: 'GET',
+            credentials: 'include'
+        });
+
+            if (response.ok) {
+                authenticatedFalse();
+                navigate('/login');
+            } else {
+                const errorData = await response.json();
+                alert(errorData.message);
+            }
+        } catch (error) {
+            console.error('Error during logout:', error);
+            alert('Errore durante il logout.');
+        }
+    }
+    
     return (
         <div className='homepage'>
             <h1>Homepage</h1>
+            <button onClick={handleLogout}>Logout</button>
         </div>
     );
 }
