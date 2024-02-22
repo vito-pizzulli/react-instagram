47436b8b8fbeb74a92cdb46490301b6cb777baf2
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-02-22 16:26:10 +0100

    Implemented user registration with navigation and State update on success.

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,19 +1,54 @@
+import { useNavigate } from 'react-router-dom';
 import '../style.css';
 
-function Registration() {
+function Registration({ authenticatedTrue }) {
+    const navigate = useNavigate();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const formDataObject = Object.fromEntries(formData.entries());
+
+        try {
+            const response = await fetch('http://localhost:3001/api/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formDataObject),
+                credentials: 'include'
+            });
+
+            if (response.ok) {
+                authenticatedTrue();
+                navigate('/');
+            } else {
+                const errorData = await response.json();
+                alert(errorData.message);
+            }
+        } catch (error) {
+            console.error('Error during registration:', error);
+            alert('Errore durante la registrazione. Riprova piú tardi.');
+        }
+    }
+
     return (
         <div className='registration'>
-            <form action="" method='POST'>
+            <form onSubmit={handleSubmit}>
+                <h2>Inserisci i tuoi dati</h2>
                 <label htmlFor="email">Email</label>
-                <input type="text" name='email' />
+                <input type="email" name='email' />
                 <label htmlFor="email">Password</label>
-                <input type="text" name='password' />
-                <label htmlFor="email">Username</label>
+                <input type="password" name='password' />
+                <label htmlFor="username">Username</label>
                 <input type="text" name='username' />
-                <label htmlFor="email">Nome</label>
+                <label htmlFor="firstname">Nome</label>
                 <input type="text" name='firstname' />
-                <label htmlFor="email">Cognome</label>
+                <label htmlFor="lastname">Cognome</label>
                 <input type="text" name='lastname' />
+                <button type='submit'>Registrati</button>
+                <button type='reset'>Resetta i campi</button>
             </form>
         </div>
     )
