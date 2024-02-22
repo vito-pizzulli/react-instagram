8f8562c2b8072bb22792a486a51e38481b745814
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-02-22 11:20:22 +0100

    Added basic structure to Login component.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,30 @@
+import { useNavigate } from 'react-router-dom';
 import '../style.css';
 
 function Login() {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/registration');
+    };
+
     return (
-        <div>
-            <h1>Login Form</h1>
-            <h1>Registration Form</h1>
+        <div className='authentication'>
+            <div className='login-area'>
+                <form action="" method='POST'>
+                    <h2>Login</h2>
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name='email' />
+                    <label htmlFor="password">Password</label>
+                    <input type="text" name='password' />
+                    <button>Accedi</button>
+                </form>
+                <button>Accedi con Google</button>
+            </div>
+            <div className='registration-area'>
+                <h2>Non sei ancora registrato?</h2>
+                <button onClick={handleClick}>Registrati ora</button>
+            </div>
         </div>
     );
 }
