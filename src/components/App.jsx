fb75110f2da6e150c9c90da71d2554e489273dc6
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-01 12:13:55 +0100

    Implemented redirect to CompleteRegistration for incomplete profiles (users logged with Google oAuth) and centralized error/state management.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,16 @@ import Header from './Header';
 import Home from './Home';
 import Login from './Login';
 import Registration from "./Registration";
+import CompleteRegistration from "./CompleteRegistration";
 import '../style.css';
 
 function App() {
   const [isAuthenticated, setAuthenticated] = useState(false);
   const [authUserInfo, setAuthUserInfo] = useState(null);
   const [confirmMessage, setConfirmMessage] = useState('');
+  const serverUrl = 'http://localhost:3001/';
+  const [serverInternalError, setServerInternalError] = useState('');
+  const [serverValidationErrors, setServerValidationErrors] = useState([]);
 
   const getStatus = async () => {
     try {
@@ -29,29 +33,70 @@ useEffect(() => {
     getStatus();
 }, []);
 
+const requiresCompleteRegistration = () => {
+  return isAuthenticated && (!authUserInfo.username || !authUserInfo.name || !authUserInfo.profile_pic_url);
+};
+
   return (
     <Router>
       <div>
         <Header />
         <main>
           <Routes>
-            <Route path="/" element={isAuthenticated ? <Home
-              setAuthenticated={setAuthenticated}
-              authUserInfo={authUserInfo}
-              confirmMessage={confirmMessage}
-              setConfirmMessage={setConfirmMessage}
-            /> : <Navigate to="/login" replace />} />
-            <Route path="/login" element={!isAuthenticated ? <Login
-              setAuthenticated={setAuthenticated}
-              setAuthUserInfo={setAuthUserInfo}
-              setConfirmMessage={setConfirmMessage}
-              confirmMessage={confirmMessage}
-            /> : <Navigate to="/" replace />} />
-            <Route path="/registration" element={<Registration
-              setAuthenticated={setAuthenticated}
-              setAuthUserInfo={setAuthUserInfo}
-              setConfirmMessage={setConfirmMessage}
-            />} />
+            <Route path="/" element={
+              requiresCompleteRegistration() ?
+              <Navigate to="/complete-registration" replace /> :
+              isAuthenticated ?
+              <Home
+                serverUrl={serverUrl}
+                serverInternalError={serverInternalError}
+                setServerInternalError={setServerInternalError}
+                setAuthenticated={setAuthenticated}
+                authUserInfo={authUserInfo}
+                confirmMessage={confirmMessage}
+                setConfirmMessage={setConfirmMessage}
+              /> : <Navigate to="/login" replace />}
+            />
+
+            <Route path="/login" element={
+              !isAuthenticated ?
+              <Login
+                serverUrl={serverUrl}
+                serverInternalError={serverInternalError}
+                setServerInternalError={setServerInternalError}
+                setAuthenticated={setAuthenticated}
+                setAuthUserInfo={setAuthUserInfo}
+                confirmMessage={confirmMessage}
+                setConfirmMessage={setConfirmMessage}
+              /> : <Navigate to="/" replace />}
+            />
+
+            <Route path="/registration" element={
+              <Registration
+                serverUrl={serverUrl}
+                serverInternalError={serverInternalError}
+                setServerInternalError={setServerInternalError}
+                serverValidationErrors={serverValidationErrors}
+                setServerValidationErrors={setServerValidationErrors}
+                setAuthenticated={setAuthenticated}
+                setAuthUserInfo={setAuthUserInfo}
+                setConfirmMessage={setConfirmMessage}
+              />} 
+            />
+            
+            <Route path="/complete-registration" element={
+              <CompleteRegistration
+                serverUrl={serverUrl}
+                serverInternalError={serverInternalError}
+                setServerInternalError={setServerInternalError}
+                serverValidationErrors={serverValidationErrors}
+                setServerValidationErrors={setServerValidationErrors}
+                setAuthenticated={setAuthenticated}
+                authUserInfo={authUserInfo}
+                setAuthUserInfo={setAuthUserInfo}
+                setConfirmMessage={setConfirmMessage}
+              />}
+            />
           </Routes>
         </main>
       </div>
