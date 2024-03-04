5be789c3c6150de57e43c237be46096625ca45a5
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-04 12:06:33 +0100

    Removed user infos from Home component.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,45 +1,14 @@
-import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
-import { useErrors } from '../contexts/ErrorsContext';
 import '../style.css';
 
 function Home() {
-    const { serverUrl, setAuthenticated, authUserInfo, confirmMessage, setConfirmMessage } = useAuth();
-    const { serverInternalError, setServerInternalError } = useErrors();
-    const navigate = useNavigate();
-
-    async function handleLogout(event) {
-        event.preventDefault();
-
-        try {
-            const response = await fetch(`${serverUrl}api/logout`, { method: 'GET',
-            credentials: 'include'
-            });
-            const data = await response.json();
-            if (!response.ok) {
-                setServerInternalError(data.message || 'Si é verificato un errore non specificato.');
-                return;
-            }
-            setServerInternalError('');
-            setConfirmMessage(data.message);
-            setAuthenticated(false);
-            navigate('/login');
-        } catch (err) {
-            console.error(err);
-        }
-    }
+    const { confirmMessage } = useAuth();
     
     return (
         <div className='homepage'>
             {confirmMessage && <p>{confirmMessage}</p>}
 
-            <h2>Ciao, {authUserInfo.username || 'username'}!</h2>
-            <p>Email: {authUserInfo.email || 'email'}</p>
-            <p>Nome: {authUserInfo.name || 'name'}</p>
-            <img src={`${serverUrl}${authUserInfo.profile_pic_url}`} alt="Profile Pic" />
-
-            {serverInternalError && <p>{serverInternalError}</p>}
-            <button onClick={handleLogout}>Logout</button>
+            <h2>Homepage</h2>
         </div>
     );
 }
