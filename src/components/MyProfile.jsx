b7271d42172375cf6e17f15613c0d95d001428ec
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-04 12:09:50 +0100

    Created MyProfile component, containing all authenticated user info.

---
+++ src/components/MyProfile.jsx
@@ -0,0 +1,51 @@
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import { useErrors } from '../contexts/ErrorsContext';
+import '../style.css';
+
+function MyProfile() {
+    const { serverUrl, setAuthenticated, authUserInfo, setConfirmMessage } = useAuth();
+    const { serverInternalError, setServerInternalError } = useErrors();
+    const navigate = useNavigate();
+
+    function handleSettingsNavigation() {
+        navigate('settings');
+    }
+
+    async function handleLogout(event) {
+        event.preventDefault();
+
+        try {
+            const response = await fetch(`${serverUrl}api/logout`, { method: 'GET',
+            credentials: 'include'
+            });
+            const data = await response.json();
+            if (!response.ok) {
+                setServerInternalError(data.message || 'Si é verificato un errore non specificato.');
+                return;
+            }
+            setServerInternalError('');
+            setConfirmMessage(data.message);
+            setAuthenticated(false);
+            navigate('/login');
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    
+    return (
+        <div className='homepage'>
+
+            <h2>{authUserInfo.username || 'username'}</h2>
+            <p>{authUserInfo.name || 'name'}</p>
+            <img src={`${serverUrl}${authUserInfo.profile_pic_url}`} alt="Profile Pic" />
+            <p>{authUserInfo.bio || 'Nessuna bio inserita.'}</p>
+            <button onClick={handleSettingsNavigation}>Modifica i tuoi dati</button>
+
+            {serverInternalError && <p>{serverInternalError}</p>}
+            <button onClick={handleLogout}>Logout</button>
+        </div>
+    );
+}
+
+export default MyProfile;
