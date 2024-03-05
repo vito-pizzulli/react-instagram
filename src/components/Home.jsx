10b0ad33190314ecefca0d290e1f9f14e820fa17
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-05 15:57:51 +0100

    Added new button in Home component to navigate to AddPost view.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import '../style.css';
 
 function Home() {
-    const { confirmMessage } = useAuth();
+    const { confirmMessage, setConfirmMessage } = useAuth();
+    const navigate = useNavigate();
+
+    function handleAddPostNavigation() {
+        setConfirmMessage('');
+        navigate('/add-post');
+    };
     
     return (
         <div className='homepage'>
             {confirmMessage && <p>{confirmMessage}</p>}
-
+            <button onClick={handleAddPostNavigation}>Nuovo Post</button>
             <h2>Homepage</h2>
         </div>
     );
