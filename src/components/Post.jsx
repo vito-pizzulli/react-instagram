1318c4ec07c204bbff8819844cdde6736340b374
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-07 15:08:56 +0100

    Implemented created_at formatted in italian date format and printed in each Post.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,24 @@
 import { useAuth } from '../contexts/AuthContext';
+import moment from 'moment';
+import 'moment/locale/it';
 import '../style.css';
 
-function Post({username, location, image_url, description}) {
+function Post({username, location, image_url, description, created_at}) {
     const { serverUrl } = useAuth();
+
+    function formatDate(date) {
+        moment.locale('it');
+        return moment(date).format('D MMMM YYYY');
+    };
+
+    const formattedDate = formatDate(created_at);
 
     return (
         <div className='post'>
             <a href={`/${username}`}>{username}</a>
             <p>{location}</p>
             <img src={`${serverUrl}${image_url}`} alt="Post Pic" />
+            <p>{formattedDate}</p>
             <p>{description}</p>
         </div>
     )
