cc3b907a2850a967887f6bfd2d5211fea3676e8d
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-07 10:56:29 +0100

    Implemented fetch and display of all post cards in MyProfile (inside PostCardsContainer component).

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,12 +1,37 @@
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { useErrors } from '../contexts/ErrorsContext';
+import Loading from "./Loading";
+import PostCardsContainer from './PostCardsContainer';
 import '../style.css';
 
 function MyProfile() {
     const { serverUrl, setAuthenticated, authUserInfo, confirmMessage, setConfirmMessage } = useAuth();
     const { serverInternalError, setServerInternalError } = useErrors();
+    const [postCards, setPostCards] = useState([]);
+    const [postCardsLoading, setPostCardsLoading] = useState(false);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        setPostCardsLoading(true);
+        const getUserPosts = async () => {
+            try {
+                const response = await fetch(`${serverUrl}api/userPosts`, {
+                method: 'GET',
+                credentials: 'include',
+                });
+                const result = await response.json();
+                setPostCardsLoading(false);
+                setPostCards(result);
+            } catch (err) {
+                console.error(err);
+                setServerInternalError('Si è verificato un errore durante il recupero dei post.');
+            }
+        };
+        
+        getUserPosts();
+    }, [ serverUrl, setServerInternalError ]);
 
     function handleSettingsNavigation() {
         navigate('settings');
@@ -36,14 +61,18 @@ function MyProfile() {
     return (
         <div className='my-profile'>
             {confirmMessage && <p>{confirmMessage}</p>}
+            {serverInternalError && <p>{serverInternalError}</p>}
             <h2>{authUserInfo.username || 'username'}</h2>
             <p>{authUserInfo.name || 'name'}</p>
             <img src={`${serverUrl}${authUserInfo.profile_pic_url}?timestamp=${new Date().getTime()}`} alt="Profile Pic" />
             <p>{authUserInfo.bio || 'Nessuna bio inserita.'}</p>
             <button onClick={handleSettingsNavigation}>Modifica i tuoi dati</button>
-
-            {serverInternalError && <p>{serverInternalError}</p>}
             <button onClick={handleLogout}>Logout</button>
+            { !postCardsLoading ?
+                <PostCardsContainer
+                    postCards={postCards}
+                />
+            : <Loading />}
         </div>
     );
 }
