413397269a36d93a30d73b3e40d87950d320f94d
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-11 16:49:06 +0100

    Implemented new async function in post show to delete the single post.

--- src/components/ShowPost.jsx
+++ src/components/ShowPost.jsx
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { useErrors } from '../contexts/ErrorsContext';
 import Loading from './Loading';
 import '../style.css';
 
 function ShowPost() {
-    const { serverUrl } = useAuth();
+    const { serverUrl, setConfirmMessage } = useAuth();
     const { setServerInternalError } = useErrors();
     const [post, setPost] = useState({});
     const [postLoading, setPostLoading] = useState(false);
     const { username, slug } = useParams();
+    const navigate = useNavigate();
+
 
     useEffect(() => {
         setPostLoading(true);
@@ -39,6 +41,30 @@ function ShowPost() {
         getSinglePost();
     }, [ username, slug, serverUrl, setServerInternalError ]);
 
+    async function handlePostDelete() {
+        const isConfirmed = window.confirm("Sei sicuro di voler eliminare questo post? Questa azione non può essere annullata.");
+
+        if (isConfirmed) {
+            try {
+                const response = await fetch(`${serverUrl}api/deletePost/${post.id}`, {
+                    method: 'DELETE',
+                    credentials: 'include'
+                });
+                const data = await response.json();
+
+                if (!response.ok) {
+                    setServerInternalError(data.message || 'Si é verificato un errore non specificato.');
+                    return;
+                }
+                setConfirmMessage(data.message);
+                navigate('/');
+    
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+
     return (
         <div className='show-post'>
             { !postLoading ?
@@ -48,6 +74,7 @@ function ShowPost() {
                         <p>{post.location}</p>
                         <img src={`${serverUrl}${post.image_url}`} alt="Post Pic" />
                         <p>{post.description}</p>
+                        <button type='button' onClick={handlePostDelete}>Elimina Post</button>
                     </>
                 ) : (
                     <p>Post non trovato.</p>
