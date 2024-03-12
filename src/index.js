ea6449b230968b9db5c45bde380a1ee9dfd0e95d
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-12 15:46:25 +0100

    Changed styling from css to scss.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from './components/App';
 import AppProviders from './contexts/AppProviders';
-import './style.css';
+import './style.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = createRoot(document.getElementById("root"));
