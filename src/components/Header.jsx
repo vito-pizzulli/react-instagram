bfad562408df8dc9019a1a3c39a27f8dcf71a418
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-13 11:25:14 +0100

    Styling fixes done to Header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
     };
 
     return (
-        <header className='d-flex align-items-center'>
+        <header className='d-flex align-items-center overflow-hidden'>
             <div className="container-fluid w-md-75 h-100">
                 <div className="row align-items-center h-100">
                     <div className="col-4 d-flex justify-content-center align-items-center h-100">
@@ -27,10 +27,18 @@ function Header() {
                     : null}
                     {isAuthenticated && (authUserInfo.username && authUserInfo.name && authUserInfo.profile_pic_url) ?
                         <div className={'col-4 d-flex justify-content-center align-items-center h-100'} role='button' onClick={() => handleNavigation(`/${authUserInfo.username}`)}>
-                            <img className='h-75 object-fit-contain rounded-circle' src={`${serverUrl}${authUserInfo.profile_pic_url}`} alt='Profile Pic' />
-                            <div className='d-flex flex-column justify-content-center align-content-center p-1'>
-                                <p>{authUserInfo.username}</p>
-                                <p className={styles.name}>{authUserInfo.name}</p>
+                            <div className="row h-100">
+                                <div className="col-12 col-sm-4 d-flex justify-content-center align-items-center h-100">
+                                    <div className={`${styles.profilePicContainer} h-75`}>
+                                        <img className='object-fit-cover rounded-circle w-100 h-100' src={`${serverUrl}${authUserInfo.profile_pic_url}`} alt='Profile Pic' />
+                                    </div>
+                                </div>
+                                <div className="d-none d-sm-flex col-8">
+                                    <div className='d-flex flex-column justify-content-center align-content-center p-1'>
+                                        <p>{authUserInfo.username}</p>
+                                        <p className={styles.name}>{authUserInfo.name}</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     : null}
