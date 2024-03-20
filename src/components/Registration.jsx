07278bf9888e0d07f3e91ebc43a9c03a3b9e2073
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-20 15:58:20 +0100

    Registration component styling completed.

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -3,6 +3,10 @@ import { useFormik } from 'formik';
 import * as yup from 'yup';
 import { useAuth } from '../contexts/AuthContext';
 import { useErrors } from '../contexts/ErrorsContext';
+import styles from '../assets/styles/Authentication.module.scss';
+import logo from '../assets/images/logo.png';
+import googlePlay from '../assets/images/google-play.png';
+import appStore from '../assets/images/app-store.png';
 
 const validationSchema = yup.object({
     email: yup.string()
@@ -105,6 +109,10 @@ function Registration() {
         formik.resetForm();
     };
 
+    function handleGoogleLogin() {
+        window.location.href = `${serverUrl}auth/google`;
+    };
+
     function handleLoginNavigation() {
         navigate('/login');
     };
@@ -115,77 +123,144 @@ function Registration() {
     };
 
     return (
-        <div className='registration'>
-            <h2>Inserisci i tuoi dati</h2>
-            <button onClick={handleLoginNavigation}>Indietro</button>
-            {serverInternalError && <p>{serverInternalError}</p>}
-            <ul>
-                {serverValidationErrors.map((serverValidationError, index) => (
-                    <li key={index}>{serverValidationError}</li>
-                ))}
-            </ul>
-            <form onSubmit={formik.handleSubmit}>
+        <div className={`${styles.registration} registration container-fluid`}>
+            <div className="row m-auto mb-3">
+                <div className="col-12 col-xl-5 m-auto d-flex flex-column align-items-center border border-secondary-subtle p-5">
+                    <img className='w-50 mb-4' src={logo} alt="Instagram logo" />
+                    <span className='mb-3'>Iscriviti per vedere le foto dei tuoi amici.</span>
+                    <button className='fw-semibold mb-3 btn btn-primary border-0' onClick={handleGoogleLogin}><i className="fa-brands fa-google me-2"></i> Accedi con Google</button>
+                    <div className="row w-100 mb-2">
+                        <div className="col-5">
+                            <hr />
+                        </div>
+                        <div className="col-2 text-center">
+                            <span className={`${styles.separator} fs-5`}>o</span>
+                        </div>
+                        <div className="col-5">
+                            <hr />
+                        </div>
+                    </div>
+                    {serverInternalError && <p className='alert alert-warning'>{serverInternalError}</p>}
+                    <ul>
+                        {serverValidationErrors.map((serverValidationError, index) => (
+                            <li className='alert alert-warning' key={index}>{serverValidationError}</li>
+                        ))}
+                    </ul>
 
-                <label htmlFor="email">Email</label>
-                <input
-                    type="email"
-                    name="email"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                    onBlur={formik.handleBlur}
-                />
-                {formik.touched.email && formik.errors.email ? <p>{formik.errors.email}</p> : null}
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    name="password"
-                    onChange={formik.handleChange}
-                    value={formik.values.password}
-                    onBlur={formik.handleBlur}
-                />
-                {formik.touched.password && formik.errors.password ? <p>{formik.errors.password}</p> : null}
-                
-                <label htmlFor="password_confirm">Conferma Password</label>
-                <input
-                    type="password"
-                    name="password_confirm"
-                    onChange={formik.handleChange}
-                    value={formik.values.password_confirm}
-                    onBlur={formik.handleBlur}
-                />
-                {formik.touched.password_confirm && formik.errors.password_confirm ? <p>{formik.errors.password_confirm}</p> : null}
+                    <form className='d-flex flex-column w-100 mb-3' onSubmit={formik.handleSubmit}>
+                        <div className='position-relative'>
+                            <span className={styles.placeholder}>Indirizzo e-mail</span>
+                            <input
+                                className='w-100'
+                                type="email"
+                                name="email"
+                                onChange={formik.handleChange}
+                                value={formik.values.email}
+                                onBlur={formik.handleBlur}
+                            />
+                        </div>
+                        {formik.touched.email && formik.errors.email ? <p className='alert alert-warning'>{formik.errors.email}</p> : null}
 
-                <label htmlFor="username">Username</label>
-                <input
-                    type="text"
-                    name="username"
-                    onChange={formik.handleChange}
-                    value={formik.values.username}
-                    onBlur={formik.handleBlur}
-                />
-                {formik.touched.username && formik.errors.username ? <p>{formik.errors.username}</p> : null}
+                        <div className="position-relative">
+                            <span className={styles.placeholder}>Nome e cognome</span>
+                            <input
+                                className='w-100'
+                                type="text"
+                                name="name"
+                                onChange={formik.handleChange}
+                                value={formik.values.name}
+                                onBlur={formik.handleBlur}
+                            />
+                        </div>
+                        {formik.touched.name && formik.errors.name ? <p className='alert alert-warning'>{formik.errors.name}</p> : null}
+                        
+                        <div className="position-relative">
+                            <span className={styles.placeholder}>Nome utente</span>
+                            <input
+                                className='w-100'
+                                type="text"
+                                name="username"
+                                onChange={formik.handleChange}
+                                value={formik.values.username}
+                                onBlur={formik.handleBlur}
+                            />
+                        </div>
+                        {formik.touched.username && formik.errors.username ? <p className='alert alert-warning'>{formik.errors.username}</p> : null}
+                        
+                        <div className="position-relative">
+                            <span className={styles.placeholder}>Immagine del profilo</span>
+                            <input
+                                className='w-100'
+                                type="file"
+                                name='profile_pic_url'
+                                onChange={handleFileChange}
+                                onBlur={formik.handleBlur}
+                            />
+                        </div>
+                        {formik.touched.profile_pic_url && formik.errors.profile_pic_url ? <p className='alert alert-warning'>{formik.errors.profile_pic_url}</p> : null}
+                        
+                        <div className="position-relative">
+                            <span className={styles.placeholder}>Password</span>
+                            <input
+                                className='w-100'
+                                type="password"
+                                name="password"
+                                onChange={formik.handleChange}
+                                value={formik.values.password}
+                                onBlur={formik.handleBlur}
+                            />
+                        </div>
+                        {formik.touched.password && formik.errors.password ? <p className='alert alert-warning'>{formik.errors.password}</p> : null}
+                        
+                        <div className="position-relative">
+                            <span className={styles.placeholder}>Conferma password</span>
+                            <input
+                                className='w-100'
+                                type="password"
+                                name="password_confirm"
+                                onChange={formik.handleChange}
+                                value={formik.values.password_confirm}
+                                onBlur={formik.handleBlur}
+                            />
+                        </div>
+                        {formik.touched.password_confirm && formik.errors.password_confirm ? <p className='alert alert-warning'>{formik.errors.password_confirm}</p> : null}
 
-                <label htmlFor="name">Nome</label>
-                <input
-                    type="text"
-                    name="name"
-                    onChange={formik.handleChange}
-                    value={formik.values.name}
-                    onBlur={formik.handleBlur}
-                />
-                {formik.touched.name && formik.errors.name ? <p>{formik.errors.name}</p> : null}
-
-                <label htmlFor="profile_pic_url">Immagine del Profilo</label>
-                <input
-                    type="file"
-                    name='profile_pic_url'
-                    onChange={handleFileChange}
-                    onBlur={formik.handleBlur}
-                />
-                {formik.touched.profile_pic_url && formik.errors.profile_pic_url ? <p>{formik.errors.profile_pic_url}</p> : null}
-                <button type='submit'>Registrati</button>
-                <button type='reset' onClick={handleReset}>Resetta i campi</button>
-            </form>
+                        <button className='mb-3 btn btn-primary fw-semibold border-0' type='submit'>Iscriviti</button>
+                        <button className='mb-3 btn btn-primary fw-semibold border-0' type='reset' onClick={handleReset}>Resetta i campi</button>
+                    </form>
+                </div>
+            </div>
+            
+            <div className="row m-auto mb-4">
+                <div className='loginArea border border-secondary-subtle p-4 text-center col-12 col-xl-5 m-auto'>
+                    <div className="row">
+                        <div className="col-12">
+                            <span>Hai un account? </span>
+                            <span className={`${styles.loginButton} fw-semibold`} role='button' onClick={handleLoginNavigation}>Accedi</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div className={styles.appDownload}>
+                <div className="row text-center mb-3">
+                    <div className="col-12">
+                        <span>Scarica l'applicazione.</span>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6 d-flex justify-content-end">
+                    <a href="https://play.google.com/store/apps/details?id=com.instagram.android&hl=it&gl=US" target="_blank" rel="noopener noreferrer">
+                        <img src={googlePlay} alt="Download from Google Play Logo" />
+                    </a>
+                    </div>
+                    <div className="col-6 d-flex justify-content-start">
+                        <a href="https://apps.apple.com/it/app/instagram/id389801252" target="_blank" rel="noopener noreferrer">
+                            <img src={appStore} alt="Download from App Store Logo." />
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
