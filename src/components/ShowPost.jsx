0077052dec03bd0eba524e9ea44ac46c05c63f44
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-21 16:29:32 +0100

    ShowPost component styling completed.

--- src/components/ShowPost.jsx
+++ src/components/ShowPost.jsx
@@ -2,16 +2,18 @@ import { useState, useEffect } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { useErrors } from '../contexts/ErrorsContext';
+import moment from 'moment';
+import 'moment/locale/it';
 import Loading from './Loading';
+import styles from '../assets/styles/ShowPost.module.scss';
 
 function ShowPost() {
-    const { serverUrl, setConfirmMessage } = useAuth();
+    const { authUserInfo, serverUrl, setConfirmMessage } = useAuth();
     const { serverInternalError, setServerInternalError } = useErrors();
     const [post, setPost] = useState({});
     const [postLoading, setPostLoading] = useState(false);
     const { username, slug } = useParams();
     const navigate = useNavigate();
-
 
     useEffect(() => {
         setPostLoading(true);
@@ -64,20 +66,51 @@ function ShowPost() {
         }
     }
 
+    function formatDate(date) {
+        moment.locale('it');
+        return moment(date).format('D MMMM YYYY');
+    };
+
+    const formattedDate = formatDate(post.created_at);
+
     return (
-        <div className='showPost'>
-            {serverInternalError && <p>{serverInternalError}</p>}
+        <div className={`${styles.showPost} showPost container-fluid`}>
+            {serverInternalError && <p className='alert alert-warning'>{serverInternalError}</p>}
             { !postLoading ?
                 (Object.keys(post).length !== 0 ? (
-                    <>  
-                        <Link to={`/${post.username}`}>{post.username}</Link>
-                        <p>{post.location}</p>
-                        <img src={`${serverUrl}${post.image_url}`} alt="Post Pic" />
-                        <p>{post.description}</p>
-                        <button type='button' onClick={handlePostDelete}>Elimina Post</button>
-                    </>
+
+                    <div className="row">
+                        <div className="col-12 col-xxl-9 border border-secondary-subtle">
+                            <img className={`${styles.postPic}`} src={`${serverUrl}${post.image_url}`} alt="Post Pic" />
+                        </div>
+                        <div className="col-12 col-xxl-3 p-0 d-flex flex-column">
+
+                            <div className="row border border-secondary-subtle p-3 m-0">
+                                <div className="col-2">
+                                    <img className={`${styles.profilePic} object-fit-cover rounded-circle`} src={`${serverUrl}${authUserInfo.profile_pic_url}`} alt="Profile Pic" />
+                                </div>
+                                <div className="col-10 d-flex flex-column">
+                                    <Link className='text-decoration-none text-black fw-semibold' to={`/${post.username}`}>{post.username}</Link>
+                                    <span className={styles.location}>{post.location}</span>
+                                </div>
+                            </div>
+                            
+                            <div className="row border border-secondary-subtle p-3 m-0 h-100">
+                                <div className="col-2">
+                                    <img className={`${styles.profilePic} object-fit-cover rounded-circle`} src={`${serverUrl}${authUserInfo.profile_pic_url}`} alt="Profile Pic" />
+                                </div>
+                                <div className="col-10 d-flex flex-column">
+                                    <p className={`${styles.description} d-inline`}>
+                                        <Link className='text-decoration-none text-black fw-semibold' to={`/${post.username}`}>{post.username}</Link> {post.description}
+                                    </p>
+                                    <span className={`${styles.date} mb-3`}>{formattedDate}</span>
+                                    <button className={`${styles.delete} btn fw-semibold`} type='button' onClick={handlePostDelete}>Elimina</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
                 ) : (
-                    <p>Post non trovato.</p>
+                    <p className='alert alert-danger'>Post non trovato.</p>
                 ))
             : <Loading />}
         </div>
