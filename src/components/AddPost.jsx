5ac6960acd33b3753f21b85a8e411504a84bf07e
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-21 12:28:00 +0100

    AddPost component styling completed.

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -3,6 +3,7 @@ import { useFormik } from 'formik';
 import * as yup from 'yup';
 import { useAuth } from '../contexts/AuthContext';
 import { useErrors } from '../contexts/ErrorsContext';
+import styles from '../assets/styles/Forms.module.scss';
 
 const validationSchema = yup.object({
     image_url: yup.mixed()
@@ -86,47 +87,59 @@ function AddPost() {
     };
 
     return (
-        <div className='addPost'>
-            <h2>Pubblica un nuovo post</h2>
-            {serverInternalError && <p>{serverInternalError}</p>}
-            <ul>
-                {serverValidationErrors.map((serverValidationError, index) => (
-                    <li key={index}>{serverValidationError}</li>
-                ))}
-            </ul>
-            <form onSubmit={formik.handleSubmit}>
-
-                <label htmlFor="image_url">Immagine del Post</label>
-                <input
-                    type="file"
-                    name='image_url'
-                    onChange={handleFileChange}
-                    onBlur={formik.handleBlur}
-                />
-                {formik.touched.image_url && formik.errors.image_url ? <p>{formik.errors.image_url}</p> : null}
-
-                <label htmlFor="description">Descrizione</label>
-                <textarea
-                    name="description"
-                    onChange={formik.handleChange}
-                    value={formik.values.description}
-                    onBlur={formik.handleBlur}
-                />
-                {formik.touched.description && formik.errors.description ? <p>{formik.errors.description}</p> : null}
-
-                <label htmlFor="location">Luogo</label>
-                <input
-                    type="text"
-                    name="location"
-                    onChange={formik.handleChange}
-                    value={formik.values.location}
-                    onBlur={formik.handleBlur}
-                />
-                {formik.touched.location && formik.errors.location ? <p>{formik.errors.location}</p> : null}
-                
-                <button type='submit'>Pubblica Post</button>
-                <button type='reset' onClick={handleReset}>Resetta i campi</button>
-            </form>
+        <div className={`${styles.addPost} addPost container-fluid`}>
+            <div className="row m-auto">
+                <div className="col-12 col-xl-5 m-auto border border-secondary-subtle p-5 text-center">
+                    <h2 className='mb-4 fw-bold'>Crea un nuovo post</h2>
+                {serverInternalError && <p className='alert alert-warning'>{serverInternalError}</p>}
+                <ul>
+                    {serverValidationErrors.map((serverValidationError, index) => (
+                        <li className='alert alert-warning' key={index}>{serverValidationError}</li>
+                    ))}
+                </ul>
+                <form className='d-flex flex-column w-100' onSubmit={formik.handleSubmit}>
+                    
+                    <div className="position-relative">
+                        <span className={styles.placeholder}>Immagine del post</span>
+                        <input
+                            className='w-100'
+                            type="file"
+                            name='image_url'
+                            onChange={handleFileChange}
+                            onBlur={formik.handleBlur}
+                        />
+                    </div>
+                    {formik.touched.image_url && formik.errors.image_url ? <p className='alert alert-warning'>{formik.errors.image_url}</p> : null}
+                    
+                    <textarea
+                        placeholder='Scrivi una didascalia'
+                        className='w-100 mb-2'
+                        name="description"
+                        onChange={formik.handleChange}
+                        value={formik.values.description}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.description && formik.errors.description ? <p className='alert alert-warning'>{formik.errors.description}</p> : null}
+                    
+                    <div className="position-relative">
+                        <span className={styles.placeholder}>Luogo</span>
+                        <input
+                            placeholder='Es: Roma'
+                            className='w-100'
+                            type="text"
+                            name="location"
+                            onChange={formik.handleChange}
+                            value={formik.values.location}
+                            onBlur={formik.handleBlur}
+                        />
+                    </div>
+                    {formik.touched.location && formik.errors.location ? <p className='alert alert-warning'>{formik.errors.location}</p> : null}
+                    
+                    <button className='mb-3 btn btn-primary fw-semibold border-0' type='submit'>Pubblica Post</button>
+                    <button className='btn btn-primary fw-semibold border-0' type='reset' onClick={handleReset}>Resetta i campi</button>
+                </form>
+                </div>
+            </div>
         </div>
     )
 }
