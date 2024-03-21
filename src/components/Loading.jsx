764e1db263dde8bec0192a58b4370c782c8fdcdf
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-21 13:18:28 +0100

    Loading image position fixed.

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import loading from '../assets/images/loading.gif';
 
 function Loading() {
     return (
-        <div className="text-center">
+        <div className="loading text-center">
             <img src={loading} alt="Spinning circle" />
         </div>
     )
