6bedd78eb51ce98535d5fd971f4e15b1aac26ed8
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-22 10:55:03 +0100

    NotFound component styling completed.

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,6 +1,18 @@
+import styles from '../assets/styles/NotFound.module.scss';
+import logo from '../assets/images/logo.png';
+
 function NotFound() {
     return (
-        <h1>Error 404 - Not Found</h1>
+        <div className={`${styles.notFound} notFound container-fluid`}>
+            <div className="row">
+                <div className="col-12 d-flex flex-column align-items-center">
+                    <img className='w-25' src={logo} alt="Instagram Logo" />
+                    <span className={`${styles.largeWord} fw-bold text-danger`}>404</span>
+                    <span className='fw-bold fs-2 mb-5 text-danger'>Not Found</span>
+                    <span className='fw-bold fs-4'>Oops! La pagina richiesta non é stata trovata.</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
